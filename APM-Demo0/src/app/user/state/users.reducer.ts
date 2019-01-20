010b54858416a8b5ac101c6359ebec070ae55b6a
LearnedByDoing <learnedbydoing@gmail.com>
2019-01-20 22:27:58 +0100

    02 - own trial: maskUserName saved in store;

---
+++ APM-Demo0/src/app/user/state/users.reducer.ts
@@ -0,0 +1,14 @@
+export function reducer(state, action) {
+
+  switch (action.type) {
+    case 'MASK_USER_NAME':
+      console.log('existing state: ', JSON.stringify(state));
+      console.log('action: ', action);
+      return {
+        ...state,
+        maskUserName: action.payload
+      }
+    default:
+      return state;
+  }
+}
